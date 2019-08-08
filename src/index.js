ce0af8bfa8532e6a9d0bc48c261af49abb170558
Mateus Mantovani <mateus21mantovani@hotmail.com>
2019-08-07 22:51:40 -0300

    Change render method

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
@@ -28,6 +28,14 @@ class App extends React.Component {
     }
 
     return <Spinner message={'Please, accept location request'} />
+  }
+
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    )
   }
 }
 
